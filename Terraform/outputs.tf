f37e8dfa700c7f9ce618b5d2e93e465a9c49d0e3
Olamide Olaleye <olamide.olaleye@outlook.com>
2023-03-17 13:20:36 +0000

    added correct outputs for resources

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -1,3 +1,24 @@
 output "name" {
   value = azurerm_resource_group.rg
 }
+output "location" {
+  value = azurerm_resource_group.rg.location
+}
+output "id" {
+  value = azurerm_resource_group.rg.id
+  sensitive = true
+}
+output "tags" {
+  value = azurerm_resource_group.rg.tags
+}
+output "stgname" {
+  value = azurerm_storage_account.stg
+  sensitive = true
+}
+output "stglocation" {
+  value = azurerm_storage_account.stg.location
+}
+output "stgid" {
+  value = azurerm_storage_account.stg.id
+  sensitive = true
+}
