28631d93780f0eccf11569ad3158c9386c019904
Olamide Olaleye <olamide.olaleye@outlook.com>
2023-03-17 13:12:49 +0000

    initial commit

--- PowerShell/compute/CBA.ps1
+++ PowerShell/compute/CBA.ps1
@@ -13,8 +13,9 @@ if (-not $app) {
   }
 }
 $param = @{
-  ClientId              = (Get-AzADApplication -DisplayName "CAP365").AppId
+  ClientId              = "d9bc4237-d021-455d-9c7d-5523c84f00b8"
   TenantId              = "1384d15a-031b-418c-b134-46c614e3bc4e"
+  #Scopes = "AuditLog.Read.All","Directory.Read.All","user.Read.all"
   CertificateThumbprint = (Get-ChildItem -Path Cert:\CurrentUser\My | Where-Object Subject -EQ "CN=*.intheclouds365.com").Thumbprint
 }
 Connect-MgGraph @param
