88e3394dc7f9ee9ca39d6de8d093ce261335727b
Olamide Olaleye <olamide.olaleye@outlook.com>
2023-03-20 22:00:00 +0000

    Changed Code

--- PowerShell/compute/Stop-AllRunningVMs.ps1
+++ PowerShell/compute/Stop-AllRunningVMs.ps1
@@ -4,13 +4,14 @@ Function Stop-AllRunningVM{
     [Parameter(Mandatory=$false)]
     [string]$VMName,
     [Parameter(Mandatory=$false)]
-    [Switch]$StopHost
+    [Switch]$IncludingHost
   )
-  $VMName = Get-VM | Where-object {$_.State -eq "Running"} | Select-Object -ExpandProperty Name
-  Stop-VM -Name $VMName -Passthru -Force
-  if ($StopHost.IsPresent) {
+  Get-VM | where-object {$_.state -eq 'Running'} | Stop-VM -Passthru -Force
+  if ($IncludingHost) {
     Stop-Computer -Force
   }else {
-    Write-Host "All VMs have been stopped."
+    Write-Output "Host will not be stopped"
+    Write-Output "To stop the host, run Stop-AllRunningVM -IncludingHost"
+    Write-Output "All running VMs have been stopped"
   }
 }
