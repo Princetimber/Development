203bfb176d22efe68aaf391271ae91ab66f754d5
Olamide Olaleye <olamide.olaleye@outlook.com>
2023-03-21 16:53:24 +0000

    added chocolatey package

--- PowerShell/compute/psmodule.ps1
+++ PowerShell/compute/psmodule.ps1
@@ -1,4 +1,4 @@
 Install-Module PowerShellGet -Scope CurrentUser -Repository PSGallery -AllowClobber -Force
 Install-Module PowerShellGet -Scope CurrentUser -Repository PSGallery -AllowPrerelease -Force
-$n = @('az', 'az.tools.predictor', 'azureAD', 'azureADPreview', 'intuneWin32App', 'Microsoft.Graph', 'Microsoft.PowerShell.SecretManagement', 'Microsoft.PowerShell.SecretStore', 'MicrosoftTeams', 'PSScriptAnalyzer', 'PSReadLine', 'ExchangeOnlineManagement')
+$n = @('az', 'az.tools.predictor', 'azureAD', 'azureADPreview', 'intuneWin32App', 'Microsoft.Graph', 'Microsoft.PowerShell.SecretManagement', 'Microsoft.PowerShell.SecretStore', 'MicrosoftTeams', 'PSScriptAnalyzer', 'PSReadLine', 'ExchangeOnlineManagement','Chocolatey')
 $n | ForEach-Object -Parallel { Install-PSResource -Name $_ -Repository PSGallery -Scope AllUsers -Quiet }
